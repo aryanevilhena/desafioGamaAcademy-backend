eab075eb9efe3a9c8fe564cdd00087f02b471db6
Aryane <aryanevilhena@gmail.com>
2021-08-29 20:47:31 -0300

    mudança no procfile

--- src/index.js
+++ src/index.js
@@ -16,7 +16,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(require('./routes'));
-app.listen('5000', () => {
-    console.log('rodando na porta 5000')
-});
+app.listen(process.env.PORT || 5000);
 
